47de57fdf59a9ac2ff47e7141358c0acf142c30a
RadostinTasev1999 <radostintasev22@yahoo.com>
2026-06-25 09:55:39 +0300

    add hoisting.js

---
+++ hoisting.js
@@ -0,0 +1,8 @@
+// console.log(foo); // undefined
+// var foo = 'foo';
+
+// console.log(baz)
+// let baz = 'baz'; // ReferenceError: Cannot access 'baz' before initialization
+
+console.log(bar); // ReferenceError: Cannot access 'bar' before initialization
+const bar = 'bar'; // 
